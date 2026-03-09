83c02bbe8fb5fb41a7dd53af16b8ebfa23a63db6
Seb Ferraro <seb.ferraro@hotmail.co.uk>
2026-03-09 13:59:35 +0000

    Fixing smite ranks and changing cast time to ternary

--- sim/priest/smite.go
+++ sim/priest/smite.go
@@ -1,29 +1,26 @@
 package priest
 
 import (
+	"time"
+
 	"github.com/wowsims/tbc/sim/common/shared"
 	"github.com/wowsims/tbc/sim/core"
 )
 
 var SmiteRankMap = shared.SpellRankMap{
-	{Rank: 1, SpellID: 585, Cost: 13, MinDamage: 13, MaxDamage: 16, Coefficient: 0.123, CastTimeSeconds: 1.5},
-	{Rank: 2, SpellID: 591, Cost: 30, MinDamage: 28, MaxDamage: 32, Coefficient: 0.271, CastTimeSeconds: 2.0},
-	{Rank: 3, SpellID: 598, Cost: 50, MinDamage: 48, MaxDamage: 53, Coefficient: 0.554},
-	{Rank: 4, SpellID: 984, Cost: 80, MinDamage: 84, MaxDamage: 94, Coefficient: 0.714},
-	{Rank: 5, SpellID: 1004, Cost: 115, MinDamage: 136, MaxDamage: 152, Coefficient: 0.714},
-	{Rank: 6, SpellID: 6060, Cost: 160, MinDamage: 207, MaxDamage: 229, Coefficient: 0.714},
-	{Rank: 7, SpellID: 10933, Cost: 210, MinDamage: 299, MaxDamage: 331, Coefficient: 0.714},
-	{Rank: 8, SpellID: 10934, Cost: 265, MinDamage: 401, MaxDamage: 445, Coefficient: 0.714},
-	{Rank: 9, SpellID: 25363, Cost: 380, MinDamage: 539, MaxDamage: 603, Coefficient: 0.714},
-	{Rank: 10, SpellID: 25364, Cost: 410, MinDamage: 640, MaxDamage: 716, Coefficient: 0.714},
+	{Rank: 1, SpellID: 585, Cost: 20, MinDamage: 15, MaxDamage: 20, Coefficient: 0.123, CastTimeSeconds: 1.5},
+	{Rank: 2, SpellID: 591, Cost: 30, MinDamage: 28, MaxDamage: 34, Coefficient: 0.271, CastTimeSeconds: 2.0},
+	{Rank: 3, SpellID: 598, Cost: 60, MinDamage: 58, MaxDamage: 67, Coefficient: 0.554},
+	{Rank: 4, SpellID: 984, Cost: 95, MinDamage: 97, MaxDamage: 112, Coefficient: 0.714},
+	{Rank: 5, SpellID: 1004, Cost: 140, MinDamage: 158, MaxDamage: 178, Coefficient: 0.714},
+	{Rank: 6, SpellID: 6060, Cost: 185, MinDamage: 222, MaxDamage: 250, Coefficient: 0.714},
+	{Rank: 7, SpellID: 10933, Cost: 230, MinDamage: 298, MaxDamage: 335, Coefficient: 0.714},
+	{Rank: 8, SpellID: 10934, Cost: 280, MinDamage: 384, MaxDamage: 429, Coefficient: 0.714},
+	{Rank: 9, SpellID: 25363, Cost: 300, MinDamage: 422, MaxDamage: 470, Coefficient: 0.714},
+	{Rank: 10, SpellID: 25364, Cost: 385, MinDamage: 549, MaxDamage: 616, Coefficient: 0.714},
 }
 
 func (priest *Priest) registerSmiteSpell(rankConfig shared.SpellRankConfig) {
-	// Default cast time for Smite is 2.5 seconds (ranks 3+)
-	baseCastTime := core.DurationFromSeconds(2.5)
-	if rankConfig.CastTimeSeconds > 0 {
-		baseCastTime = core.DurationFromSeconds(rankConfig.CastTimeSeconds)
-	}
 
 	spell := priest.RegisterSpell(core.SpellConfig{
 		ActionID:       core.ActionID{SpellID: rankConfig.SpellID},
@@ -38,7 +35,7 @@ func (priest *Priest) registerSmiteSpell(rankConfig shared.SpellRankConfig) {
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{
 				GCD:      core.GCDDefault,
-				CastTime: baseCastTime,
+				CastTime: core.TernaryDuration(rankConfig.CastTimeSeconds > 0, core.DurationFromSeconds(rankConfig.CastTimeSeconds), 2500*time.Millisecond),
 			},
 		},
 
